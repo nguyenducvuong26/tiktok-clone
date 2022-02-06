e0c5063e66deeb4cb483caa67e09f610398ba4c9
Vuong <vuong260902@gmail.com>
2022-02-06 23:08:12 +0700

    Adding 404 Page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import classes from "./NotFoundPage.module.css";
+
+export default function NotFoundPage() {
+    return (
+        <div className={classes.container}>
+            <h1>404</h1>
+            <h2>Page not found</h2>
+        </div>
+    );
+}
